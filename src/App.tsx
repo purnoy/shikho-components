12c0c79c715bb6005be9444ebeebabb29b6508e7
Purnot <purnoy.cc@gmail.com>
2023-09-10 16:07:45 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="">
+    <div className="h-screen w-full bg-red-200">
       <Button variant="primary" color="blue" size="medium">
         Call Me
       </Button>
