24b1b5a139f78e8f5116760ca5467febc7b3ed09
Purnot <purnoy.cc@gmail.com>
2023-09-10 15:27:04 +0600

    Initial commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  color: "blue" | "pink" | "white";
+  variant: "primary" | "secondary" | "outline";
+  size: "large" | "medium" | "small" | "extra-small";
+  label?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ color, variant, size, children }) => {
+  const primaryBtn = `${
+    color === "white" ? "text-pink-500" : "text-white"
+  } bg-${color}-500 rounded-lg shadow-lg cursor-pointer`;
+  const secondaryBtn = `${
+    color === "white" ? "text-pink-500" : "text-white"
+  } bg-${color}-300 rounded-lg shadow-lg cursor-pointer`;
+  const outlineBtn = `${
+    color === "white" ? "text-pink-500" : "text-black"
+  } bg-transparent border-${color}-500 text-black rounded-lg shadow-lg cursor-pointer`;
+
+  const largeSize = "px-8 py-4 text-xl";
+  const mediumSize = "px-6 py-3 text-lg";
+  const smallSize = "px-4 py-2 text-md";
+  const extraSmallSize = "px-2 py-1 text-sm";
+
+  const variantClasses =
+    variant === "primary"
+      ? primaryBtn
+      : variant === "secondary"
+      ? secondaryBtn
+      : outlineBtn;
+
+  const sizeClasses =
+    size === "large"
+      ? largeSize
+      : size === "medium"
+      ? mediumSize
+      : size === "small"
+      ? smallSize
+      : extraSmallSize;
+
+  return (
+    <button className={`${variantClasses} ${sizeClasses}`}>{children}</button>
+  );
+};
+
+export default Button;
